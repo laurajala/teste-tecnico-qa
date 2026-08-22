8228c9e90083ab71327d34e5f03d1768ef6e59f9
Laura Ajala <lauraq2000@hotmail.com>
2026-08-22 13:42:47 -0400

    Enhance 'Carrinho' test with beforeEach and better naming
    Refactor test case to improve structure and readability.

--- cypress/e2e/carrinho.cy.js
+++ cypress/e2e/carrinho.cy.js
@@ -1,21 +1,35 @@
-describe('Carrinho', () => {
+describe('Carrinho de Compras', () => {
 
-  it('Deve adicionar produto ao carrinho', () => {
-
+  beforeEach(() => {
     cy.visit('https://automationexercise.com')
+  })
+
+  it('CT07 - Deve adicionar um produto ao carrinho com sucesso', () => {
 
     cy.contains('Products')
       .click()
 
-    cy.contains('Add to cart')
+    cy.get('.productinfo')
       .first()
-      .click({ force: true })
+      .within(() => {
+        cy.get('p')
+          .invoke('text')
+          .as('nomeProduto')
+
+        cy.contains('Add to cart')
+          .click({ force: true })
+      })
 
     cy.contains('View Cart')
       .click()
 
     cy.get('#cart_info_table')
       .should('be.visible')
+
+    cy.get('@nomeProduto').then((nomeProduto) => {
+      cy.get('#cart_info_table')
+        .should('contain.text', nomeProduto.trim())
+    })
   })
 
 })
