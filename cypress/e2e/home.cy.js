f86dd67adde1a1de7c9c88c377112c8348605529
Laura Ajala <lauraq2000@hotmail.com>
2026-08-22 13:47:24 -0400

    Change homepage URL to relative path

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -2,7 +2,7 @@ describe('Home', () => {
 
   it('Deve acessar a página inicial com sucesso', () => {
 
-    cy.visit('https://automationexercise.com')
+    cy.visit('/')
 
     cy.contains('AutomationExercise')
       .should('be.visible')
